327b0291527aed5d26ba3f7ca9ab592d89926438
sanjeev07 <csesanjeev07@gmail.com>
2016-02-02 20:23:43 +0530

    responsive design error soloved in navbar

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,110 +1,89 @@
 
-
-
-
-
-
-
-
-
-
-
+// NAVBAR STYLE SECTION
+// ==============================
 
 $(document).ready(function(){
-	$(".link").css("color","#4682B4");
+    $(".link1").css({"color":"#4682B4","font-size":"14px"});
+    $(".link2").css({"color":"#4682B4","font-size":"14px"});
+    $(".link3").css({"color":"#4682B4","font-size":"14px"});
+    $(".link4").css({"color":"#4682B4","font-size":"14px"});
+    $(".link5").css({"color":"#4682B4","font-size":"14px"});
+    $(".link6").css({"color":"#4682B4","font-size":"14px"});
+    $(".link7").css({"color":"#4682B4","font-size":"14px"});
 });
 
 $(document).ready(function(){
-$( ".link" ).on( "mouseover", function() {
-  $( this ).css({"background-color":"#4682B4","color":"white","font-size":"20px;","font-weight":"800"});
+$( ".link1" ).on( "mouseover", function() {
+  $( this ).css({"background-color":"#4682B4","color":"white","font-size":"16px","font-weight":"800"});
 });
-$( ".link" ).on( "mouseout", function() {
-  $( this ).css({"color":"#4682B4","background-color":"#F7F7F7","font-size":"14px;","font-weight":"600"});
+$( ".link2" ).on( "mouseover", function() {
+  $( this ).css({"background-color":"#4682B4","color":"white","font-size":"16px","font-weight":"800"});
+});
+$( ".link3" ).on( "mouseover", function() {
+  $( this ).css({"background-color":"#4682B4","color":"white","font-size":"16px","font-weight":"800"});
+});
+$( ".link4" ).on( "mouseover", function() {
+  $( this ).css({"background-color":"#4682B4","color":"white","font-size":"16px","font-weight":"800"});
+});
+$( ".link5" ).on( "mouseover", function() {
+  $( this ).css({"background-color":"#4682B4","color":"white","font-size":"16px","font-weight":"800"});
+});
+$( ".link6" ).on( "mouseover", function() {
+  $( this ).css({"background-color":"#4682B4","color":"white","font-size":"16px","font-weight":"800"});
+});
+$( ".link7" ).on( "mouseover", function() {
+  $( this ).css({"background-color":"#4682B4","color":"white","font-size":"16px","font-weight":"800"});
+});
+$( ".link1" ).on( "mouseout", function() {
+  $( this ).css({"color":"#4682B4","background-color":"#FDFDFD","font-size":"14px","font-weight":""});
+});
+$( ".link2" ).on( "mouseout", function() {
+  $( this ).css({"color":"#4682B4","background-color":"#FDFDFD","font-size":"14px","font-weight":""});
+});
+$( ".link3" ).on( "mouseout", function() {
+  $( this ).css({"color":"#4682B4","background-color":"#FDFDFD","font-size":"14px","font-weight":""});
+});
+$( ".link4" ).on( "mouseout", function() {
+  $( this ).css({"color":"#4682B4","background-color":"#FDFDFD","font-size":"14px","font-weight":""});
+});
+$( ".link5" ).on( "mouseout", function() {
+  $( this ).css({"color":"#4682B4","background-color":"#FDFDFD","font-size":"14px","font-weight":""});
+});
+$( ".link6" ).on( "mouseout", function() {
+  $( this ).css({"color":"#4682B4","background-color":"#FDFDFD","font-size":"14px","font-weight":""});
+});
+$( ".link7" ).on( "mouseout", function() {
+  $( this ).css({"color":"#4682B4","background-color":"#FDFDFD","font-size":"14px","font-weight":""});
 });
 });
+
+//============================================================
+
+// BODY SCROLLBAR HIDDEN
+// ==============================
+
 $(document).ready(function(){
-	$("body").css("overflow","hidden");
+  $("body").css("overflow","hidden");
 });
-//$(document).ready(function(){
-	//$("fig1").css("background-image","url("+ assets/images/download.jpg +")");
-//});
-    /*$(document).ready(function() {
-        $('#tab1').bind("mouseover", function(){
-            var col  = $(this).css("background-color");
 
-            $(this).css("background", "red");
-            $(this).css("color", "white");
+//===============================================================
 
-            $(this).bind("mouseout", function(){
-                $(this).css("background", col);
-            });    
-        });    
-    });
-    $(document).ready(function() {
-        $('#tab2').bind("mouseover", function(){
-            var color  = $(this).css("background-color");
+// DISABLE AUTO SLIDE
+// ==============================
 
-            $(this).css("background", "red");
-
-            $(this).bind("mouseout", function(){
-                $(this).css("background", color);
-            })    
-        })    
-    })*/
-       /*$(document).ready(function() {
-        $('#tab3').bind("mouseover", function(){
-            var color  = $(this).css("background-color");
-
-            $(this).css("background", "red");
-
-            $(this).bind("mouseout", function(){
-                $(this).css("background", color);
-            })    
-        })    
-    })
-
-       $(document).ready(function() {
-        $('#tab4').bind("mouseover", function(){
-            var color  = $(this).css("background-color");
-
-            $(this).css("background", "red");
-
-            $(this).bind("mouseout", function(){
-                $(this).css("background", color);
-            })    
-        })    
-    })*/
-
-
-    /*$(document).ready(function() {
-        $('#tab1').click(function(){
-        	$("#fig1").removeClass("hideall")
-        	$("#fig2").addClass("hideall")
-        	$("#fig3").addClass("hideall")
-        	$("#fig4").addClass("hideall")
-        });
-         $('#tab2').click(function(){
-        	$("#fig2").removeClass("hideall")
-        	$("#fig1").addClass("hideall")
-        	$("#fig3").addClass("hideall")
-        	$("#fig4").addClass("hideall")
-        });
-        $('#tab3').click(function(){
-        	$("#fig3").removeClass("hideall")
-        	$("#fig1").addClass("hideall")
-        	$("#fig2").addClass("hideall")
-        	$("#fig4").addClass("hideall")
-        });
-        $('#tab4').click(function(){
-        	$("#fig4").removeClass("hideall")
-        	$("#fig1").addClass("hideall")
-        	$("#fig3").addClass("hideall")
-        	$("#fig2").addClass("hideall")
-
-        });
-    });*/
 $(document).ready(function(){
      $("#myCarousel").carousel({
          interval : false
      });
 });
+
+//================================================================
+
+//SCROLLBAR STYLE
+//====================
+$(document).ready(function(){ 
+
+    $("html").niceScroll({styler:"fb",cursorcolor:"#000"});
+
+  });
+//==================================================================
